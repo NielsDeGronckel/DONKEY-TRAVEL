e43cbb34067d12bf9cb2359f4d32ff206a09f293
Lukas Sliva <lukas.sliva.developer@gmail.com>
2023-10-16 20:48:36 +0200

    create in oop

--- Classes/Table.php
+++ Classes/Table.php
@@ -9,7 +9,36 @@ class Table {
 
     }
 
-    //create (not necessary)
+    //create new row for generator table
+    public function tableCreate($table_name, $column_names) {
+        require 'database/pureConnect.php';
+
+        $insert_values = [];
+        foreach ($column_names as $column) {
+            $value = isset($_POST[$column]) ? $_POST[$column] : null;
+            
+            // Check if the column name contains 'password'
+            if (strpos($column, 'password') !== false) {
+                $value = password_hash($value, PASSWORD_DEFAULT);
+            }
+    
+            $insert_values[] = $value;
+        }
+    
+        // Create the SQL query dynamically
+        $sql = "INSERT INTO $table_name (" . implode(', ', $column_names) . ") VALUES (" . rtrim(str_repeat('?, ', count($column_names)), ', ') . ")";
+            
+        try {
+            $stmt = $conn->prepare($sql);
+            $stmt->execute($insert_values);
+            $_SESSION['message'] =  "Data inserted successfully";
+            $Table = ucfirst($table_name);
+            $filename = "adminTable" . $Table;
+            header("Location: " . $filename . "#bottom");    
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
 
     //read for the generator
     public function tableRead($tabelnaam) {
--- adminCreate.php
+++ adminCreate.php
@@ -1,36 +1,13 @@
 <?php 
-require 'database/database.php';
+require 'Classes/Table.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $table_name = $_POST['table_name'];
     $column_names = explode(',', $_POST['column_names']);
     
     // Construct the SQL query to insert data
-    $insert_values = [];
-    foreach ($column_names as $column) {
-        $value = isset($_POST[$column]) ? $_POST[$column] : null;
-        
-        // Check if the column name contains 'password'
-        if (strpos($column, 'password') !== false) {
-            $value = password_hash($value, PASSWORD_DEFAULT);
-        }
-
-        $insert_values[] = $value;
-    }
-
-    // Create the SQL query dynamically
-    $sql = "INSERT INTO $table_name (" . implode(', ', $column_names) . ") VALUES (" . rtrim(str_repeat('?, ', count($column_names)), ', ') . ")";
-        
-    try {
-        $stmt = $conn->prepare($sql);
-        $stmt->execute($insert_values);
-        $_SESSION['message'] =  "Data inserted successfully";
-        $Table = ucfirst($table_name);
-        $filename = "adminTable" . $Table;
-        header("Location: " . $filename . "#bottom");    
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
+    $createTable = new Table();
+    $createTable->tableCreate($table_name, $column_names);
 }
 
 
